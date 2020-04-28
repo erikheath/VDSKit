5fe65d1b0520cd43ba681819bc835febce63a43d
Erikheath Thomas <erikheath@tether360.com>
2020-04-28 13:37:37 -0700

    Error management description.

--- VDSKit/VDSErrorConstants.h
+++ VDSKit/VDSErrorConstants.h
@@ -7,6 +7,82 @@
 //
 
 
+/*
+ Error Management in VDSKit
+ 
+ VDSKit makes liberal use of error reporting and error chaining to provide
+ consistent, informative, and timely feedback for developers, and where appropriate,
+ for end users. To accomplish this, VDSKit follows three general patterns for error
+ reporting:
+ 
+ 1. Most methods accept the address of an error pointer that can be set to
+    point to a valid error object when one occurs.
+ 
+ 2. Methods that accept an error pointer address return a BOOL value to
+    indicate whether the message sender should check for an error.
+ 
+ 3. For those classes that suport error logging, methods use a consistent naming
+    convention to indicate if an error will be recorded internally as well as be
+    reported, or if it will only be reported to the sender.
+ 
+    Methods beginning with 'can', 'add', 'set', or 'remove' do not record to an
+    object or class's internal error log. Note however, that methods called
+    internally by 'can', 'add', 'set', or 'remove' methods may log errors to a
+    class or object's internal error log.
+ 
+    All other methods that accept an error pointer will generally log an error
+    as well as report it to the sender. Methods that vary from this pattern
+    are documented as doing so with the underlying rationale.
+ 
+ 4. Some methods do not accept an error pointer, but instead provide other
+    means of determining if an error has occurred. This is generaly the case
+    with methods whose interals are executed asynchronously. In these cases,
+    either a delegate will be called with an error object or a completion
+    handler can be provided that will receive an error object if an error
+    occurs. In either case, for classes or objects that log errors, the
+    error will be logged internally as indicated by the preceding rule(s).
+ 
+ In addition to error reporting, VDSKit makes use of error chaining when reporting
+ errors. Error chaining links a series of errors together using NSErrors userInfo
+ property with the NSUnderlyingError key associated with an error object that underlies
+ the main error.
+ 
+ Because error chaining occurs when multiple errors happen as the result of
+ a method executing, a chain can often be matched to a call stack, enabling much
+ faster error resolution. To make error chaining easier to use, VDSKit errors
+ include more information than is typical in most frameworks. This includes:
+ 
+ 1. Error Location: The selector name is included to identify where in a call stack
+    an error originated. Use the key VDSErrorLocationKey.
+ 
+ 2. Parameter Value Descriptions: For each argument submitted to the method, the
+    parameter name and description for objects or values for non objects is included
+    with the error. Use the key VDSErrorLocationParametersKey.
+ 
+ 3. Error Domain: For VDSKit, this is the VDSKitErrorDomain.
+ 
+ 4. Error Code: VDSKit provides a number of error codes that indicate the actual cause
+    of the error. For example, a nil argument was encountered where a non-nil argument
+    was expected.
+ 
+ 5. VDSKit Subsystem Error Key: VDSKit provides a number of error keys that correspond
+    to its subsystems such as VDSOperationErrorKey and VDSCacheErrorKey. These error
+    keys provide an interpretation of the cause of the error within the context of the
+    subsystem and the method. Where possible, a recovery suggestion is provided. This
+    suggestion is not appropriate for end users as it represents an internal diagnostic.
+    This value is repeated using the NSDebugDescriptionErrorKey for convenience.
+ 
+ 6. Multiple Errors Report: In the event that multiple errors occur on multiple threads
+    as part of a method's execution, VDSKit reports the individual errors using the
+    VDSMultipleErrorsKey with an array of the error objects. For this type of error,
+    the NSUnderlyingErrorsKey is not included in the userInfo dictionary.
+ 
+ Where possible, additional standard Cocoa Error keys are used to provide as rich a
+ diagnostic report as possible. Methods that produce user facing error messages are
+ documented as doing so.
+ 
+ */
+
 #import <Foundation/Foundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
@@ -15,14 +91,19 @@ NS_ASSUME_NONNULL_BEGIN
 
 FOUNDATION_EXPORT NSString* const VDSKitErrorDomain;
 
-typedef NS_ENUM(NSUInteger, VDSCacheErrorCode) {
-    VDSEntryNotFound = 1,
-    VDSUnexpectedNilArgument = 2,
-    VDSUnableToRemoveObject = 3,
+typedef NS_ENUM(NSUInteger, VDSKitErrorCode) {
+    VDSUnknownError = 1,
+    VDSMultipleErrors = 2,
+    VDSUnexpectedNilArgument = 3,
 };
 
-
 #pragma mark - VDSKit Cache Errors -
+
+typedef NS_ENUM(NSUInteger, VDSCacheErrorCode) {
+    VDSEntryNotFound = 1,
+    VDSUnableToRemoveObject = 2,
+};
+
 
 typedef NSString* const VDSCacheErrorKey;
 
