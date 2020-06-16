d065869acef522175005d74b19bd1d0e62927989
Erikheath Thomas <erikheath@tether360.com>
2020-06-16 16:23:01 -0700

    Added additional testing for DatabaseCache initialization to cover basic add/remove behaviors.

--- VDSKitTests/DatabaseCacheTests/VDSDatabaseCacheTests.m
+++ VDSKitTests/DatabaseCacheTests/VDSDatabaseCacheTests.m
@@ -67,24 +67,99 @@
     VDSDatabaseCache* cache = [[VDSDatabaseCache alloc] initWithConfiguration:config];
     cache.defaultExpirationInterval = 300;
     
-    // Confirm the configuration
+    /// Confirm the configuration
     XCTAssertTrue(cache.configuration.expiresObjects);
+    XCTAssertEqual(cache.defaultExpirationInterval, 300);
+    XCTAssertEqual(cache.configuration.evictionInterval, 0);
     
+    
+    /// Add a single object to the cache
     NSObject* object = [NSObject new];
-    NSString* key1 = @"testObj1";
-    [cache setObject:object forKey:key1];
+    NSString* key = @"testObj";
+    [cache setObject:object forKey:key];
     
-    XCTAssertEqual(object, [cache objectForKey:key1]);
+    XCTAssertEqual(object, [cache objectForKey:key]);
     XCTAssertFalse([[cache trackedObjects] containsObject:object]);
     XCTAssertTrue([[cache untrackedObjects] containsObject:object]);
     XCTAssertTrue([[cache allObjects] containsObject:object]);
-    XCTAssertTrue([[cache allKeys] containsObject:key1]);
-    XCTAssertFalse([[cache trackedKeys] containsObject:key1]);
-    XCTAssertTrue([[cache untrackedKeys] containsObject:key1]);
+    XCTAssertTrue([[cache allKeys] containsObject:key]);
+    XCTAssertFalse([[cache trackedKeys] containsObject:key]);
+    XCTAssertTrue([[cache untrackedKeys] containsObject:key]);
     XCTAssertEqual([[cache trackedObjectsAndKeys] count], 0);
     XCTAssertEqual([[cache untrackedObjectsAndKeys] count], 1);
-    XCTAssertEqualObjects([[[cache untrackedObjectsAndKeys] allKeys] firstObject], key1);
+    XCTAssertEqualObjects([[[cache untrackedObjectsAndKeys] allKeys] firstObject], key);
     XCTAssertEqualObjects([[[cache untrackedObjectsAndKeys] allValues] firstObject], object);
+    XCTAssertEqual([[cache allObjectsAndKeys] count], 1);
+    XCTAssertEqualObjects([[[cache allObjectsAndKeys] allKeys] firstObject], key);
+    XCTAssertEqualObjects([[[cache allObjectsAndKeys] allValues] firstObject], object);
+    XCTAssertFalse([cache incrementUsageCount:object]);
+    XCTAssertFalse([cache decrementUsageCount:object]);
+    
+    /// Empty the cache
+    [cache removeObjectForKey:key];
+    
+    XCTAssertFalse([[cache trackedObjects] containsObject:object]);
+    XCTAssertFalse([[cache untrackedObjects] containsObject:object]);
+    XCTAssertFalse([[cache allObjects] containsObject:object]);
+    XCTAssertFalse([[cache allKeys] containsObject:key]);
+    XCTAssertFalse([[cache trackedKeys] containsObject:key]);
+    XCTAssertFalse([[cache untrackedKeys] containsObject:key]);
+    XCTAssertEqual([[cache trackedObjectsAndKeys] count], 0);
+    XCTAssertEqual([[cache untrackedObjectsAndKeys] count], 0);
+    XCTAssertEqual([[cache allObjectsAndKeys] count], 0);
+
+    /// Fill the cache with multiple objects
+    NSObject* object1 = [NSObject new];
+    NSObject* object2 = [NSObject new];
+    NSObject* object3 = [NSObject new];
+    
+    NSString* key1 = @"testKey1";
+    NSString* key2 = @"testKey2";
+    NSString* key3 = @"testKey3";
+    
+    [cache setObject:object1 forKey:key1];
+    [cache setObject:object2 forKey:key2];
+    [cache setObject:object3 forKey:key3];
+    
+    XCTAssertEqual(object1, [cache objectForKey:key1]);
+    XCTAssertEqual(object2, [cache objectForKey:key2]);
+    XCTAssertEqual(object3, [cache objectForKey:key3]);
+    XCTAssertNotEqualObjects(object1, [cache objectForKey:key2]);
+    XCTAssertNotEqualObjects(object2, [cache objectForKey:key3]);
+    XCTAssertNotEqualObjects(object3, [cache objectForKey:key1]);
+
+    NSSet* objectSet = [NSSet setWithArray:@[object1, object2, object3]];
+    NSSet* keySet = [NSSet setWithArray:@[key1, key2, key3]];
+    XCTAssertFalse([objectSet isSubsetOfSet:[NSSet setWithArray:[cache trackedObjects]]]);
+    XCTAssertTrue([objectSet isSubsetOfSet:[NSSet setWithArray:[cache untrackedObjects]]]);
+    XCTAssertTrue([objectSet isSubsetOfSet:[NSSet setWithArray:[cache allObjects]]]);
+    XCTAssertTrue([keySet isSubsetOfSet:[NSSet setWithArray:[cache allKeys]]]);
+    XCTAssertFalse([keySet isSubsetOfSet:[NSSet setWithArray:[cache trackedKeys]]]);
+    XCTAssertTrue([keySet isSubsetOfSet:[NSSet setWithArray:[cache untrackedKeys]]]);
+    XCTAssertEqual([[cache trackedObjectsAndKeys] count], 0);
+    XCTAssertEqual([[cache untrackedObjectsAndKeys] count], 3);
+    XCTAssertEqualObjects([NSSet setWithArray:[[cache untrackedObjectsAndKeys] allKeys]], keySet);
+    XCTAssertEqualObjects([NSSet setWithArray:[[cache untrackedObjectsAndKeys] allValues]], objectSet);
+    XCTAssertEqual([[cache allObjectsAndKeys] count], 3);
+    XCTAssertEqualObjects([NSSet setWithArray:[[cache allObjectsAndKeys] allKeys]], keySet);
+    XCTAssertEqualObjects([NSSet setWithArray:[[cache allObjectsAndKeys] allValues]], objectSet);
+    XCTAssertFalse([cache incrementUsageCount:object2]);
+    XCTAssertFalse([cache decrementUsageCount:object3]);
+
+    
+    /// Remove all objects
+    [cache removeAllObjects];
+    
+    XCTAssertFalse([[cache trackedObjects] containsObject:object]);
+    XCTAssertFalse([[cache untrackedObjects] containsObject:object]);
+    XCTAssertFalse([[cache allObjects] containsObject:object]);
+    XCTAssertFalse([[cache allKeys] containsObject:key]);
+    XCTAssertFalse([[cache trackedKeys] containsObject:key]);
+    XCTAssertFalse([[cache untrackedKeys] containsObject:key]);
+    XCTAssertEqual([[cache trackedObjectsAndKeys] count], 0);
+    XCTAssertEqual([[cache untrackedObjectsAndKeys] count], 0);
+    XCTAssertEqual([[cache allObjectsAndKeys] count], 0);
+
 }
 
 - (void)testCustomInitWithUsageTracking
